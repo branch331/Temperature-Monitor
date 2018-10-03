37e199f2476541a24b82c8eeef3db0c5ff66ff7b
branch331 <34431991+branch331@users.noreply.github.com>
2018-10-03 15:22:40 -0500

    New file directory
    made new file directory for final submission

---
+++ TemperatureMonitor/HardwareViewModel.cs
@@ -0,0 +1,79 @@
+﻿using NationalInstruments.SystemConfiguration;
+using System.Linq;
+
+namespace NationalInstruments.Examples.BoardTemperatureMonitor
+{
+    internal class HardwareViewModel 
+    {
+        public HardwareViewModel(ProductResource resource, double temperatureLimit)
+        { 
+            UserAlias = resource.UserAlias;
+            NumberOfExperts = resource.Experts.Count;
+            ExpertResourceName = resource.Experts[0].ResourceName;
+            ExpertProgrammaticName = resource.Experts[0].ExpertProgrammaticName;
+
+            LimitReached = false;
+
+            try
+            {
+                TemperatureSensor[] sensors = resource.QueryTemperatureSensors(SensorInfo.Reading);
+
+                Temperature = " ";
+
+                for (int i = 0; i < sensors.Length; i++)
+                {
+                    if (i != sensors.Length - 1)
+                    {
+                        Temperature += sensors[i].Reading.ToString("0.00") + ", ";
+                    }
+                    else
+                    {
+                        Temperature += sensors[i].Reading.ToString("0.00");
+                    }
+                }
+
+                LimitReached = sensors.Any(s => s.Reading > temperatureLimit);
+            }
+            catch
+            {
+                Temperature = "N/A";
+            }    
+        }
+
+        public string UserAlias
+        {
+            get;
+            private set;
+        }
+
+        public string ExpertResourceName
+        {
+            get;
+            private set;
+        }
+
+        public string ExpertProgrammaticName
+        {
+            get;
+            private set;
+        }
+
+        public string Temperature
+        {
+            get;
+            private set;
+        }
+
+        public int NumberOfExperts
+        {
+            get;
+            private set;
+        }
+
+        public bool LimitReached
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ TemperatureMonitor/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NationalInstruments.Examples.BoardTemperatureMonitor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            worker = new BoardTemperatureMonitorWorker();
+            mainGrid.DataContext = worker;
+            TemperatureLimitBox.Text = "50";
+            TemperatureSlider.Value = 50;
+        }
+
+        private BoardTemperatureMonitorWorker worker;
+
+        private void OnRunAuditClick(object sender, RoutedEventArgs e)
+        {
+            worker.StartRunAudit(passwordBox.Password);
+        }
+
+        private void OnStopButtonClick(object sender, RoutedEventArgs e)
+        {
+            worker.StopMonitor = true;
+        }
+    }
+}
